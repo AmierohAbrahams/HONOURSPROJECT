46b66c3ef91dc1a2c04785d2f767a823a908008a
AJ Smit <albertus.smit@gmail.com>
2018-11-19 10:02:48 +0200

    Edits.

--- functions/temp_r2.R
+++ functions/temp_r2.R
@@ -1,20 +1,20 @@
 ### R2 values for the temp data###
 
-temp_wave_R2_ <- function(df){
+temp_wave_R2_ <- function(df) {
   results <- df %>% 
-    select(-index, -src, -date, -length, -c(num:depth)) %>%
+    dplyr::select(-index, -src, -date, -length, -c(num:depth)) %>%
     gather(key = variable, 
            value = value,
            -cluster, -site, -temp) %>%
-    group_by(cluster, site, variable) %>%
+    dplyr::group_by(cluster, site, variable) %>%
     na.omit() %>% 
     nest() %>%
-    mutate(model = purrr::map(data, ~lm(temp ~ value, data = .))) %>%
+    dplyr::mutate(model = purrr::map(data, ~lm(temp ~ value, data = .))) %>%
     unnest(model %>% purrr::map(glance)) %>%
-    select(cluster:variable, adj.r.squared, p.value) %>% 
-    arrange(cluster, site) %>% 
-    mutate(adj.r.squared = round(adj.r.squared, 2),
-           p.value = round(p.value, 4)) %>% 
+    dplyr::select(cluster:variable, adj.r.squared, p.value) %>% 
+    dplyr::arrange(cluster, site) %>% 
+    dplyr::mutate(adj.r.squared = round(adj.r.squared, 2),
+                  p.value = round(p.value, 4)) %>% 
     dplyr::rename("R^2" = adj.r.squared) %>% 
     dplyr::rename(P = p.value) %>% 
     dplyr::rename(Cluster = cluster) %>% 
--- functions/temp_wave_r2.R
+++ functions/temp_wave_r2.R
@@ -1,7 +1,7 @@
 ##### Creating a func####
 
 # A function that runs a linear model on each metric of the combined data and get the R^2 value
-temp_wave_R2 <- function(df){
+temp_wave_R2 <- function(df) {
   results <- df %>% 
     dplyr::select(-index, -src, -date, -length, -c(num:depth), -temp) %>%
     gather(key = variable, 
@@ -15,6 +15,6 @@ temp_wave_R2 <- function(df){
     dplyr::select(cluster:variable, adj.r.squared, p.value) %>% 
     dplyr::arrange(cluster, site) %>% 
     dplyr::mutate(adj.r.squared = round(adj.r.squared, 2),
-           p.value = round(p.value, 4))
+                  p.value = round(p.value, 4))
   return(results)
 }
